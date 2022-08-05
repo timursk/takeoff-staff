71acc66bb5169ddc07ad994f19fb7eed60fa39af
unknown <timur.dev@mail.ru>
2022-08-06 00:05:12 +0300

    feat: setup lint, prettier & husky configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ROUTES } from './constants/routes';
-import { Main } from './pages/Main';
-import { Contacts } from './pages/Contacts';
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ROUTES } from "./constants/routes";
+import { Main } from "./pages/Main";
+import { Contacts } from "./pages/Contacts";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={ROUTES.MAIN} element={<Main/>} />
+        <Route path={ROUTES.MAIN} element={<Main />} />
         <Route path={ROUTES.CONTACTS} element={<Contacts />} />
       </Routes>
     </BrowserRouter>
